a6ecd989c23b249125ba481ef0a9e29844394e06
lusissy <lourdessissy@gmail.com>
2025-05-27 13:19:14 -0300

    cont

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,19 @@
+namespace EspacioCalculadora
+{
+    public class Calculadora
+    {
+         public float Resultado;
+         public double Termino;
+
+         void Sumar(double Termino){
+            
+
+         }
+        void Restar(double Termino){}
+        void Multiplicar(double Termino){}
+        void Dividir(double Termino){}
+        void Limpiar(){}
+
+
+    }
+}
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EspacioCalculadora;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Calculadora nuevo=new Calculadora();
